ff49ff980c95e09e1589f3b3280f10660808811b
ikanop <adrianulrix@gmail.com>
2024-12-30 17:13:56 +0100

    added scrollbutton on page 2 // removed scrollbar

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function changePage(pageID) {
     const mainContent = document.getElementById("main-content")
     const container = document.getElementById("container")
     const gridContainer = document.getElementById("grid-container")
+    const page22 = document.getElementById("page22")
 
     const pages = {
         page1: {
@@ -47,9 +48,25 @@ function changePage(pageID) {
     if (pageID === "page2") {
         container.classList.add("page2");
         gridContainer.classList.add("page2");
+        page22.classList.add("page2");
     } else {
         container.classList.remove("page2");
         gridContainer.classList.remove("page2");
+        page22.classList.remove("page2");
     }
 }
 changePage("page1")
+
+function scrollPage22() {
+    const footer = document.querySelector("footer")
+
+    if (footer) {
+        footer.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+        })
+    }
+}
+
+const button = document.getElementById("page22")
+button.addEventListener("click", scrollPage22)
