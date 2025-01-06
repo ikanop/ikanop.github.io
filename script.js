2a61f50ce2fda2ce70e6373aec523578f4d5c29e
ikanop <adrianulrix@gmail.com>
2025-01-06 12:59:03 +0100

    test

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function changePage(pageID) {
-    const Header = document.getElementById("header");
+    const header = document.getElementById("header");
     const mainContent = document.getElementById("main-content")
     const container = document.getElementById("container")
     const gridContainer = document.getElementById("grid-container")
-    const page2_1 = document.getElementById("page2_1")
-    const flip_card_container = document.getElementById("flip-card-container")
+    const flipCardContainer = document.getElementById("flip-card-container")
+    const RNG = document.getElementById("RNG")
 
     const pages = {
         page1: {
@@ -43,31 +43,31 @@ function changePage(pageID) {
         }
     };
 
-    Header.innerHTML = pages[pageID].title;
+    header.innerHTML = pages[pageID].title;
     mainContent.innerHTML = pages[pageID].content;
 
     if (pageID === "page2") {
         container.classList.add("page2");
         gridContainer.classList.add("page2");
-        page2_1.classList.add("page2");
-        flip_card_container.classList.add("page2");
+        RNG.classList.add("page2")
+        scrollToFooter.classList.add("page2");
+        flipCardContainer.classList.add("page2");
     } else {
         container.classList.remove("page2");
         gridContainer.classList.remove("page2");
-        page2_1.classList.remove("page2");
-        flip_card_container.classList.remove("page2");
-
+        RNG.classList.remove("page2")
+        scrollToFooter.classList.remove("page2");
+        flipCardContainer.classList.remove("page2");
     }
-
-    //if (pageID === "page1") {
-        //body.classList.add("page1");
-    //} else {
-        //body.classList.remove("page1");
-    //}
 }
+
+
+
 changePage("page1")
 
-function scrollPage2_1() {
+
+
+function scroll() {
     const footer = document.querySelector("footer")
 
     if (footer) {
@@ -78,8 +78,10 @@ function scrollPage2_1() {
     }
 }
 
-const button = document.getElementById("page2_1")
-button.addEventListener("click", scrollPage2_1)
+const button = document.getElementById("scrollToFooter")
+button.addEventListener("click", scroll)
+
+
 
 function flipCard(front) {
     const innerCard = front.closest(".flip-card-inner");
@@ -94,4 +96,23 @@ function flipCard(front) {
             innerCard.classList.remove("fast-flip");
         }, 500)
     }, 3000)
+}
+
+
+function randomNumber() {
+    const outputElement = document.getElementById("RNG-number")
+
+    const index = Math.floor(Math.random() * 100);
+    outputElement.innerHTML = `RNG: ${index}`;
+}
+
+
+
+function randomTxt() {
+    const outputElement = document.getElementById("RNG-txt")
+
+    const texts = ["Hello", "World", "How are you?", "Random Text", "Have a great day!"];
+
+    const index = Math.floor(Math.random() * texts.length);
+    outputElement.innerHTML = texts[index]
 }
