0083cbb2812922d0807f68b15c1df1b3520a1cac
ikanop <adrianulrix@gmail.com>
2025-01-28 21:09:05 +0100

    added new splash text

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ function changePage(pageID) {
                         <p>Content</p>
                     </div>
                     <div class="info-spacing">
-                        <h2>Header</h2>
-                        <p>Content</p>
+                        <h2>Random Quote</h2>
+                        <div id="RNG-txt"><div>
                     </div>
                 `
         },
@@ -45,6 +45,10 @@ function changePage(pageID) {
 
     header.innerHTML = pages[pageID].title;
     mainContent.innerHTML = pages[pageID].content;
+
+    if (pageID === "page1") {
+        randomTxt()
+    }
 
     if (pageID === "page2") {
         container.classList.add("page2");
@@ -111,7 +115,30 @@ function randomNumber() {
 function randomTxt() {
     const outputElement = document.getElementById("RNG-txt")
 
-    const texts = ["Hello", "World", "How are you?", "Random Text", "Have a great day!"];
+    const texts =  ["Believe you can, and you're halfway there.",
+    "The best way to predict the future is to create it.",
+    "Do what you can, with what you have, where you are.",
+    "Your limitation—it's only your imagination.",
+    "Dream it. Wish it. Do it.",
+    "Don't watch the clock; do what it does. Keep going.",
+    "Success doesn’t come from what you do occasionally, but what you do consistently.",
+    "Push yourself, because no one else is going to do it for you.",
+    "Why don’t scientists trust atoms? Because they make up everything!",
+    "Life is short. Smile while you still have teeth.",
+    "I told my computer I needed a break, and now it won’t stop sending me KitKats!",
+    "I’m not lazy, I’m just on energy-saving mode.",
+    "The unexamined life is not worth living.",
+    "Happiness is not something ready-made. It comes from your own actions.",
+    "In the middle of every difficulty lies opportunity.",
+    "We are what we repeatedly do. Excellence, then, is not an act, but a habit.",
+    "Do or do not, there is no try.",
+    "I am Groot.",
+    "It's dangerous to go alone! Take this.",
+    "Not all those who wander are lost.",
+    "Adventure is out there!",
+    "Keep calm and code on.",
+    "RNG gods, bless this roll!",
+    "Always look on the bright side of life."];
 
     const index = Math.floor(Math.random() * texts.length);
     outputElement.innerHTML = texts[index]
